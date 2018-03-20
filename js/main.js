54ae792955a01ebc9a31a26dfe81c1b280f49354
Rebecca Ussai Henderson <rebbeccaussaihenderson@ia-mac-24-rebeccaussaihenderson.attlocal.net>
2018-03-20 07:19:29 -0500

    unit 9 project

--- js/main.js
+++ js/main.js
@@ -1,17 +1,122 @@
+
+
+
 // initialize the configuration of map
 function initMap() {
    
+ 	var styles = [
+  {
+    featureType: "all",
+    stylers: [
+       { color: "#C0C0C0" }
+    ]
+  },{
+    featureType: "road.arterial",
+    elementType: "geometry",
+    stylers: [
+       { color: "#CCFFFF" }
+    ]
+  },{
+    featureType: "landscape",
+    elementType: "labels",
+    stylers: [
+      {visibility: "off" }
+    ]
+  }
+];
     // create a new instance of a map
     // configure map with options object
     var map = new google.maps.Map(document.getElementById('map'), {
+    	style: [
+            {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+            {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+            {
+              featureType: 'administrative.locality',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'geometry',
+              stylers: [{color: '#263c3f'}]
+            },
+            {
+              featureType: 'poi.park',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#6b9a76'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry',
+              stylers: [{color: '#38414e'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#212a37'}]
+            },
+            {
+              featureType: 'road',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#9ca5b3'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry',
+              stylers: [{color: '#746855'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'geometry.stroke',
+              stylers: [{color: '#1f2835'}]
+            },
+            {
+              featureType: 'road.highway',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#f3d19c'}]
+            },
+            {
+              featureType: 'transit',
+              elementType: 'geometry',
+              stylers: [{color: '#2f3948'}]
+            },
+            {
+              featureType: 'transit.station',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#d59563'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'geometry',
+              stylers: [{color: '#17263c'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.fill',
+              stylers: [{color: '#515c6d'}]
+            },
+            {
+              featureType: 'water',
+              elementType: 'labels.text.stroke',
+              stylers: [{color: '#17263c'}]
+            }
+          ],
     	center: {lat: 40.8054491, lng: -73.9654415},
     	zoom: 12,
-    	scrollwheel: false
+    	scrollwheel: false,
  	});
 
-
-    
+  
 }
+$( window ).on('load', function() {
 
 initMap();
+
+});
 
